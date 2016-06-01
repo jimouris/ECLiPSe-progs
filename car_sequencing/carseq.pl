8e5199a1bc76963797e89e0736ccd0aeea0573b7
Dimitris Mouris <jimmouris@hotmail.com>
2016-06-02 01:16:31 +0300

    Minor updates

--- car_sequencing/carseq.pl
+++ car_sequencing/carseq.pl
@@ -6,19 +6,19 @@
 %% Constraint library ic %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% classes([1,1,2,2,2,2]).
-%% options([2/1/[1,0,0,0,1,1],
-%%         3/2/[0,0,1,1,0,1],
-%%         3/1/[1,0,0,0,1,0],
-%%         5/2/[1,1,0,1,0,0],
-%%         5/1/[0,0,1,0,0,0]]).
+classes([1,1,2,2,2,2]).
+options([2/1/[1,0,0,0,1,1],
+        3/2/[0,0,1,1,0,1],
+        3/1/[1,0,0,0,1,0],
+        5/2/[1,1,0,1,0,0],
+        5/1/[0,0,1,0,0,0]]).
 
-classes([5,3,7,1,10,2,11,5,4,6,12,1,1,5,9,5,12,1]).
-options([2/1/[1,1,1,0,1,1,1,1,0,0,0,0,0,1,0,0,0,0],
-         3/2/[1,1,1,1,1,0,0,0,1,1,1,0,0,0,1,0,0,0],
-         3/1/[0,0,1,1,0,0,0,1,0,0,1,1,1,0,0,0,0,1],
-         5/2/[0,1,0,1,0,0,1,0,0,1,0,0,1,0,0,0,1,0],
-         5/1/[1,0,0,0,0,1,0,0,1,0,0,1,0,0,0,1,0,0]]).
+%% classes([5,3,7,1,10,2,11,5,4,6,12,1,1,5,9,5,12,1]).
+%% options([2/1/[1,1,1,0,1,1,1,1,0,0,0,0,0,1,0,0,0,0],
+%%          3/2/[1,1,1,1,1,0,0,0,1,1,1,0,0,0,1,0,0,0],
+%%          3/1/[0,0,1,1,0,0,0,1,0,0,1,1,1,0,0,0,0,1],
+%%          5/2/[0,1,0,1,0,0,1,0,0,1,0,0,1,0,0,0,1,0],
+%%          5/1/[1,0,0,0,0,1,0,0,1,0,0,1,0,0,0,1,0,0]]).
 
 %% classes([6,3,2,2,1,4,1,5,2,3]).
 %% options([2/1/[1,1,1,0,0,0,0,0,1,0],
@@ -42,18 +42,22 @@ carseq(S) :-
     constraint(S, Opts),
     search(S, 0, first_fail, indomain, complete, []).
 
+%% Constrain each variable(index) to appear in solution Ci times.
 occ_constraint(_, _, []).
 occ_constraint(CarLine, Idx, [Ci|Clss]) :-
     occurrences(Idx, CarLine, Ci),
     Index is Idx+1,
     occ_constraint(CarLine, Index, Clss).
 
+%% Never exist in each M consecutive cars, more than K that require option O.
 constraint(_, []).
 constraint(CarLine, [M/K/O|Opts]) :-
     calcPos(O, 1, Positions),
-    sequence_total(0, 1000, 0, K, M, CarLine, Positions),
+    sequence_total(0, 100000, 0, K, M, CarLine, Positions),
     constraint(CarLine, Opts).
 
+%% Convert a list of 0,1 to a indexes of 1 list.
+%% e.g. calcPos([0,1,1,0,0,1], 1, R), R = [2,3,6] 
 calcPos([], _, []).
 calcPos([0|Ls], I, Rs) :-
     Ind is I+1,
